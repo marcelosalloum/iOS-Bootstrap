47c019027d4d6baeb5f9ad9c9a22a938c2862a48
Marcelo Salloum dos Santos <marcelo@ckl.io>
2018-12-27 19:13:26 -0200

    Created XCode project

---
+++ CKL iOS Challenge/CKL iOS Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CKL iOS Challenge
+//
+//  Created by Marcelo Salloum dos Santos on 27/12/18.
+//  Copyright © 2018 Marcelo Salloum dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
