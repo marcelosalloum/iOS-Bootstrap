381efa85d9655dd55d31fb3d175afeee05661643
Marcelo Salloum dos Santos <marcelo@ckl.io>
2018-12-28 15:51:03 -0200

    Started `ArticleTableViewController`

---
+++ CKL iOS Challenge/CKL iOS Challenge/Article/ArticleTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  ArticleTableViewCell.swift
+//  CKL iOS Challenge
+//
+//  Created by Marcelo Salloum dos Santos on 28/12/18.
+//  Copyright © 2018 Marcelo Salloum dos Santos. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var articleImageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tagsLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
