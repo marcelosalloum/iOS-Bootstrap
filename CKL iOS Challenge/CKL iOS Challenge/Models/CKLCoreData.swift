a8ff2b28e69077ac5adc9c0f9f7f471fc8f21a27
Marcelo Salloum dos Santos <marcelo@ckl.io>
2019-01-10 10:42:33 -0200

    Started mapping tags to article

--- CKL iOS Challenge/CKL iOS Challenge/Models/CKLCoreData.swift
+++ CKL iOS Challenge/CKL iOS Challenge/Models/CKLCoreData.swift
@@ -42,6 +42,9 @@ class CKLCoreData: NSObject {
                 importArticle(from: from, toObject: article)
             }
         case String(describing: Tag.self):
+            if let tag = toObject as? Tag {
+                importTag(from: from, toObject: tag)
+            }
             print("Tag")
         default:
             print("None")
@@ -75,7 +78,24 @@ class CKLCoreData: NSObject {
         if let website = from["website"].string {
             toObject.website = website
         }
+        if let tags = from["tags"].array {
+            do {
+                guard let tagObjects = try Tag.importObjects(tags, context: CKLCoreData.context, idKey: "id", save: false) else { return }
+                let tagsSet = NSSet.init(array: tagObjects)
+                toObject.addToTags(tagsSet)
+            } catch let error as NSError {
+                // Error handling [Context Save]
+                print("ERROR: \(error.localizedDescription)")  // TODO: turn on/off verbose option
+            }
+        }
     }
     
-//    static func importJSON(from: JSON, toObject: Self) {
+    func importTag(from: JSON, toObject: Tag) {
+        if let id = from["id"].int16 {
+            toObject.id = id
+        }
+        if let label = from["label"].string {
+            toObject.label = label
+        }
+    }
 }
