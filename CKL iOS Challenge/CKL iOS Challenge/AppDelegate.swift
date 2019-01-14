4e459fc6769843d3756d73a279c7cd9b55d50c26
Marcelo Salloum dos Santos <marcelo@ckl.io>
2019-01-14 09:34:45 -0200

    Installed **Crashlytics**

--- CKL iOS Challenge/CKL iOS Challenge/AppDelegate.swift
+++ CKL iOS Challenge/CKL iOS Challenge/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import CoreData
+import Fabric
+import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        Fabric.with([Crashlytics.self])
         return true
     }
 
