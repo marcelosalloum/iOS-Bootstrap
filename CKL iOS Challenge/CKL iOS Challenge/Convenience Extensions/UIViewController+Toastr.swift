c08e32a6eb778ad5b1c662562cbe25b7bb7d3e08
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-02-04 12:06:21 -0200

    Implemented much simpler toastr message system

---
+++ CKL iOS Challenge/CKL iOS Challenge/Convenience Extensions/UIViewController+Toastr.swift
@@ -0,0 +1,23 @@
+//
+//  UIViewController+Toastr.swift
+//  CKL iOS Challenge
+//
+//  Created by Marcelo Salloum dos Santos on 04/02/19.
+//  Copyright © 2019 Marcelo Salloum dos Santos. All rights reserved.
+//
+
+import UIKit
+
+
+extension UIViewController {
+    
+    /// Displays a simple toastr in the ViewController
+    func toastr(_ message: String, delay: Double = 2.0) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .actionSheet)
+        self.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + delay) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
+}
