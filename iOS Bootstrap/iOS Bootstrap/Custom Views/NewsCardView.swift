34224c0d1077477c59bd5f484b0d0c90a7c1d252
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-02-13 13:45:37 -0200

    Removed unused code in file NewsCardView.swift

--- iOS Bootstrap/iOS Bootstrap/Custom Views/NewsCardView.swift
+++ iOS Bootstrap/iOS Bootstrap/Custom Views/NewsCardView.swift
@@ -39,39 +39,3 @@ open class NewsCardView: UIView, NibFileOwnerLoadable {
         // Do your view code-customization here
     }
 }
-
-//import UIKit
-//
-//@IBDesignable class MyCustomView: UIView {
-//    var contentView: UIView?
-//    @IBInspectable var nibName: String?
-//
-//    override func awakeFromNib() {
-//        super.awakeFromNib()
-//        xibSetup()
-//    }
-//
-//    func xibSetup() {
-//        guard let view = loadViewFromNib() else { return }
-//        view.frame = bounds
-//        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-//        addSubview(view)
-//        contentView = view
-//    }
-//
-//    func loadViewFromNib() -> UIView? {
-//        guard let nibName = nibName else { return nil }
-//        let bundle = Bundle(for: type(of: self))
-//        let nib = UINib(nibName: nibName, bundle: bundle)
-//        return nib.instantiate(
-//            withOwner: self,
-//            options: nil).first as? UIView
-//    }
-//
-//    override func prepareForInterfaceBuilder() {
-//        xibSetup()
-//        contentView?.prepareForInterfaceBuilder()
-//        self.backgroundColor = UIColor.random()
-//        super.prepareForInterfaceBuilder()
-//    }
-//}
