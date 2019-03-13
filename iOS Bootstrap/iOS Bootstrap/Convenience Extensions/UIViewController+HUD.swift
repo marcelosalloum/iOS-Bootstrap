1a09d81510900f200bf99310983142c4546012d5
Marcelo Salloum dos Santos <marcelosalloum@gmail.com>
2019-03-13 12:21:05 -0300

    Improved the UIViewController+HUD protocol

--- iOS Bootstrap/iOS Bootstrap/Convenience Extensions/UIViewController+HUD.swift
+++ iOS Bootstrap/iOS Bootstrap/Convenience Extensions/UIViewController+HUD.swift
@@ -11,7 +11,7 @@ import SVProgressHUD
 
 /// This allows a ViewController to call a HUD (Head-Up Display) anytime.
 /// It's encapsulated within this protocol, so a HUD lib can be easily replaced
-protocol HUD {
+protocol HUD: class {
     /// Shows a success message HUD
     func showSuccessHUD(message: String?, delay: TimeInterval)
     /// Shows a error message HUD
@@ -20,40 +20,57 @@ protocol HUD {
     func showInfoHUD(message: String?, delay: TimeInterval)
     /// Shows a progress HUD
     func showProgressHUD(progress: Float, statusMessage: String?)
+    /// Shows a loading HUD with an optional `message`
+    func showLoadingHUD(message: String?)
     /// Dismisses a HUD
     func dismissHUD(delay: TimeInterval)
 }
 
 extension HUD where Self: UIViewController {
+
+    /// Configures the HUD appearance
     fileprivate func configHud() {
-        SVProgressHUD.setBorderColor(.gray20)
+        SVProgressHUD.setBorderColor(.lightGray)
         SVProgressHUD.setBorderWidth(0.5)
         SVProgressHUD.setBackgroundColor(UIColor(255, 255, 255, 0.3))
+        SVProgressHUD.setDefaultMaskType(.black)
     }
 
-    func showSuccessHUD(message: String?, delay: TimeInterval = 2) {
+    /// Shows a success message HUD
+    func showSuccessHUD(message: String?, delay: TimeInterval = 3) {
         configHud()
         SVProgressHUD.showSuccess(withStatus: message)
         SVProgressHUD.dismiss(withDelay: delay)
     }
 
-    func showErrorHUD(message: String?, delay: TimeInterval = 2) {
+    /// Shows a error message HUD
+    func showErrorHUD(message: String?, delay: TimeInterval = 3) {
         configHud()
         SVProgressHUD.showError(withStatus: message)
         SVProgressHUD.dismiss(withDelay: delay)
     }
 
-    func showInfoHUD(message: String?, delay: TimeInterval = 2) {
+    /// Shows an info message HUD
+    func showInfoHUD(message: String?, delay: TimeInterval = 3) {
         configHud()
         SVProgressHUD.showInfo(withStatus: message)
         SVProgressHUD.dismiss(withDelay: delay)
     }
 
+    /// Shows a progress HUD
     func showProgressHUD(progress: Float, statusMessage: String? = nil) {
         configHud()
         SVProgressHUD.showProgress(progress, status: statusMessage)
     }
 
+    /// Shows a loading HUD with an optional `message`
+    func showLoadingHUD(message: String?) {
+        configHud()
+        SVProgressHUD.show(withStatus: message)
+        SVProgressHUD.dismiss(withDelay: 10)
+    }
+
+    /// Dismisses a HUD
     func dismissHUD(delay: TimeInterval = 0) {
         SVProgressHUD.dismiss(withDelay: delay)
     }
